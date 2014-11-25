cfe0cb27dcf8816fd47c1ac893ffc8682aefd0ce
MFloresr <mrsitofull@gmail.com>
2014-11-25 14:11:46 +0100

    gg

--- Matricules/src/main/java/net/mflores/Matricules/App.java
+++ Matricules/src/main/java/net/mflores/Matricules/App.java
@@ -1,22 +1,16 @@
 package net.mflores.Matricules;
-
 import java.io.*;
-
-import net.xaviersala.llegirfitxer.App;
-
-
-
 
 public class App{
 	private static final String FICHERO = "/matricula.txt";
-	private static final String NOENCONTRADO = "El fichero"+ FICHERO + "no ha sido encontrado";
-	private static final String ERRORABRIR = "Error al Abrir el fichero"+ FICHERO;
+	private static final String ERRORCERRAR= "Error al cerrar el fichero"+ FICHERO;
 	private static final String ERRORFICHERO = "No se puede leer el fichero" + FICHERO;
+	private static final String NOENCONTRADO = "El fichero"+ FICHERO + "no ha sido encontrado";
 
     public static void main( String[] args ){
 
     	BufferedReader inb = null;
-    	InputStream in =App.class.getResourceAsStream((FICHERO);
+    	InputStream in =App.class.getResourceAsStream(FICHERO);
 
     	if (in != null){
     		Reader r = new InputStreamReader(in);
@@ -25,61 +19,28 @@ public class App{
     		String linia;
     		try{
     			linia = inb.readLine();
-    			while( (linia != null){
-    	               System.out.println(linia);
+    			while( (linia != null)){
+    				linia.split(linia);
+    				System.out.println(linia);
+    				linia = inb.readLine();
     			}
-    		}
+    		}catch(IOException e){
+    	          System.out.println(ERRORFICHERO);
+    	          e.printStackTrace();
+    	    } finally {
+    	          try {
+    	        	  inb.close();
+    	          } catch (IOException e) {
+    	        	  System.out.println(ERRORCERRAR);
+    	          }
+    	    }
+    	}else{
+    		System.out.println(NOENCONTRADO);
     	}
-        try  {
-           matricula=new FileReader(App.class.getResource("/matricula.txt").getPath());
-
-           int c = 0 ;
-
-
-
-
-           }
-      }catch (FileNotFoundException e) {
-    	  e.printStackTrace();
-      }catch (IOException e){
-    	  e.printStackTrace();
-      }finally{
-    	  try{
-    		  matricula.close();
-    	  }catch(IOException e){
-    		  e.printStackTrace();
-    	  }
-      }
-
-
     }
 }
 
-      try {
-        linia = inb.readLine();
-
-        while (linia != null) {
-          System.out.println(linia);
-          linia = inb.readLine();
-        }
 
 
 
-      } catch (IOException e1) {
-        System.out.println(PROBLEMESFITXER);
-        e1.printStackTrace();
-      } finally {
-        try {
-          inb.close();
-        } catch (IOException e) {
-          System.out.println(PROBLEMESTANCAR);
-        }
-      }
-
-
-    } else {
-      System.out.println(NOTROBAT);
-    }
-
-  }
 
