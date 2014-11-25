aa3d37a3ca9a0395c157f05e0a87b6b79ffd1f82
MFloresr <mrsitofull@gmail.com>
2014-11-25 12:17:19 +0100

    fdf

--- Matricules/src/main/java/net/mflores/Matricules/App.java
+++ Matricules/src/main/java/net/mflores/Matricules/App.java
@@ -2,25 +2,40 @@ package net.mflores.Matricules;
 
 import java.io.*;
 
+import net.xaviersala.llegirfitxer.App;
 
 
 
-public class App
-{
-    public static void main( String[] args )
-    {
 
-    	FileReader matricula=null;
-    	StringBuffer str=new StringBuffer();
+public class App{
+	private static final String FICHERO = "/matricula.txt";
+	private static final String NOENCONTRADO = "El fichero"+ FICHERO + "no ha sido encontrado";
+	private static final String ERRORABRIR = "Error al Abrir el fichero"+ FICHERO;
+	private static final String ERRORFICHERO = "No se puede leer el fichero" + FICHERO;
+
+    public static void main( String[] args ){
+
+    	BufferedReader inb = null;
+    	InputStream in =App.class.getResourceAsStream((FICHERO);
+
+    	if (in != null){
+    		Reader r = new InputStreamReader(in);
+    		inb = new BufferedReader(r);
+
+    		String linia;
+    		try{
+    			linia = inb.readLine();
+    			while( (linia != null){
+    	               System.out.println(linia);
+    			}
+    		}
+    	}
         try  {
            matricula=new FileReader(App.class.getResource("/matricula.txt").getPath());
 
            int c = 0 ;
 
-           while( (c = matricula.read()) !=-1){
-        	   str.append((char)c);
-        	   //str.reverse();
-               System.out.println(str);
+
 
 
            }
@@ -39,3 +54,32 @@ public class App
 
     }
 }
+
+      try {
+        linia = inb.readLine();
+
+        while (linia != null) {
+          System.out.println(linia);
+          linia = inb.readLine();
+        }
+
+
+
+      } catch (IOException e1) {
+        System.out.println(PROBLEMESFITXER);
+        e1.printStackTrace();
+      } finally {
+        try {
+          inb.close();
+        } catch (IOException e) {
+          System.out.println(PROBLEMESTANCAR);
+        }
+      }
+
+
+    } else {
+      System.out.println(NOTROBAT);
+    }
+
+  }
+
